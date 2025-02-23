ed04df166118eb5b4436639f425049e1b3638073
NiteeshRohela <116974878+Niteeshruhela@users.noreply.github.com>
2025-02-23 21:03:49 +0530

    Update MyApplications.jsx

--- frontend/vite-project/src/components/Application/MyApplications.jsx
+++ frontend/vite-project/src/components/Application/MyApplications.jsx
@@ -18,7 +18,7 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employer") {
         axios
-          .get("https://jobportal-api-gold.vercel.app/application/employer/getapplication", {
+          .get("https://jobportal-api-rho.vercel.app/application/employer/getapplication", {
             withCredentials: true,
           })
           .then((res) => {
@@ -26,7 +26,7 @@ const MyApplications = () => {
           });
       } else {
         axios
-          .get("https://jobportal-api-gold.vercel.app/application/jobseeker/getapplication", {
+          .get("https://jobportal-api-rho.vercel.app/application/jobseeker/getapplication", {
             withCredentials: true,
           })
           .then((res) => {
@@ -45,7 +45,7 @@ const MyApplications = () => {
   const deleteApplication = (id) => {
     try {
       axios
-        .delete(`https://jobportal-api-gold.vercel.app/application/delete/${id}`, {
+        .delete(`https://jobportal-api-rho.vercel.app/application/delete/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
