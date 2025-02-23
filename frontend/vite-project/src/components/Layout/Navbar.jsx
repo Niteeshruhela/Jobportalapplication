a1ed8edcaefe2f8498590c5b47b5a4a93d11e6e2
NiteeshRohela <116974878+Niteeshruhela@users.noreply.github.com>
2025-02-23 14:54:51 +0530

    Update Navbar.jsx

--- frontend/vite-project/src/components/Layout/Navbar.jsx
+++ frontend/vite-project/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://jobportal-api-gold.vercel.app/user/logout",
         {
           withCredentials: true,
         }
