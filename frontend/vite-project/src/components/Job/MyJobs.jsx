484499845ea853407eaa44520c8ee119bb6002c4
NiteeshRohela <116974878+Niteeshruhela@users.noreply.github.com>
2025-02-23 14:53:51 +0530

    Update MyJobs.jsx

--- frontend/vite-project/src/components/Job/MyJobs.jsx
+++ frontend/vite-project/src/components/Job/MyJobs.jsx
@@ -17,7 +17,7 @@ const MyJobs = () => {
     const fetchJobs = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/job/getmyjobs",
+          "https://jobportal-api-gold.vercel.app/job/getmyjobs",
           { withCredentials: true }
         );
         setMyJobs(data.job);
@@ -47,7 +47,7 @@ const MyJobs = () => {
   const handleUpdateJob = async (jobId) => {
     const updatedJob = myJobs.find((job) => job._id === jobId);
     await axios
-      .put(`http://localhost:4000/api/v1/job/update/${jobId}`, updatedJob, {
+      .put(`https://jobportal-api-gold.vercel.app/job/update/${jobId}`, updatedJob, {
         withCredentials: true,
       })
       .then((res) => {
@@ -62,7 +62,7 @@ const MyJobs = () => {
   //Function For Deleting Job
   const handleDeleteJob = async (jobId) => {
     await axios
-      .delete(`http://localhost:4000/api/v1/job/delete/${jobId}`, {
+      .delete(`https://jobportal-api-gold.vercel.app/job/delete/${jobId}`, {
         withCredentials: true,
       })
       .then((res) => {
